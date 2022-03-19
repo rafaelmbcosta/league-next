ff817f010f5f79e0fdc3140d22af75789d1845f9
Rafael Costa <rafaelmbcosta@gmail.com>
2022-03-19 14:53:57 -0300

    WIP: topscores

--- components/home/TopScores.js
+++ components/home/TopScores.js
@@ -1,5 +1,19 @@
-import { Box } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react';
+
+const boxes = [
+  { title: 'Disputa Mensal', link: '/scores/month' },
+  { title: 'Copa da Liga', link: '/scores/league' },
+  { title: 'Campeonato', link: '/scores/championship' },
+  { title: 'Patrimônio', link: '/scores/currency' },
+];
 
 export default function TopScores() {
-    return(<Box>TOP SCORES</Box>)
+  return (
+    <Box>
+      ???
+      {boxes.map((box) => (
+        <div>oi!</div>
+      ))}
+    </Box>
+  );
 }
